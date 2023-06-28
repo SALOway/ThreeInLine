f3f53cb6f1499b8ee9d3b8c22a7fe943ac599c1b
Yehor <zhora.levchenkoko@gmail.com>
2023-06-28 15:13:22 +0300

    Update GameBoard.cs
    Added:
    - Tile 2-dim array
    - GetTile method
    - SetTiles method

--- Assets/_Scripts/GameBoard.cs
+++ Assets/_Scripts/GameBoard.cs
@@ -8,5 +8,32 @@ using UnityEngine;
 /// </summary>
 public class GameBoard : MonoBehaviour
 {
-    
+    private Tile[,] _tilesInGrid;
+
+    /// <summary>
+    /// Returns tile at the given position on grid
+    /// </summary>
+    /// <param name="tileGridPosition"></param>
+    /// <returns></returns>
+    public Tile GetTile(Vector3Int tileGridPosition)
+    {
+        Tile tile = null;
+        if (_tilesInGrid != null)
+        {
+            bool XIsValid = tileGridPosition.x >= 0 && tileGridPosition.x < _tilesInGrid.GetLength(0);
+            bool YIsValid = tileGridPosition.y >= 0 && tileGridPosition.y < _tilesInGrid.GetLength(1);
+
+            if (XIsValid && YIsValid) tile = _tilesInGrid[tileGridPosition.x, tileGridPosition.y];
+        }
+        return tile;
+    }
+
+    /// <summary>
+    /// Set current tiles on game board
+    /// </summary>
+    /// <param name="tiles"></param>
+    public void SetTiles(Tile[,] tiles)
+    {
+        _tilesInGrid = tiles;
+    }
 }
