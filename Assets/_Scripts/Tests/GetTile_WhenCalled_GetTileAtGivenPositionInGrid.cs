87cc80ded19a9d022d151b67f0aaa3d3913933a0
Yehor <zhora.levchenkoko@gmail.com>
2023-06-28 18:28:13 +0300

    Update GetTile Test Script
    Made the code more readable and clear and removed unnecessary functionality, which now performs TestingUtilities

--- Assets/_Scripts/Tests/GetTile_WhenCalled_GetTileAtGivenPositionInGrid.cs
+++ Assets/_Scripts/Tests/GetTile_WhenCalled_GetTileAtGivenPositionInGrid.cs
@@ -9,48 +9,27 @@ namespace Tests
 {
     public class GetTile_WhenCalled_GetTileAtGivenPositionInGrid
     {
-        (int width, int height) _boardSize;
         GameBoard _gameBoard;
-        Tile[,] _tiles;
+        Tile[,] _tilesReference;
         Vector3Int _tileGridPosition;
 
         [SetUp]
         public void Setup()
         {
-            _boardSize = (3, 3);
-            _gameBoard = CreateGameBoard();
-            _tiles = CreateTiles(_boardSize.width, _boardSize.height);
+            _gameBoard = TestingUtilites.CreateGameBoard();
+            _tilesReference = TestingUtilites.CreateTiles(3, 3);
             _tileGridPosition = new Vector3Int(2, 1, 0);
-        }
-
-        private GameBoard CreateGameBoard()
-        {
-            GameObject gameBoardObject = new GameObject();
-            GameBoard gameBoard = gameBoardObject.AddComponent<GameBoard>();
-            return gameBoard;
-        }
-        private Tile[,] CreateTiles(int width, int height)
-        {
-            Tile[,] tiles = new Tile[width, height];
-
-            for (int x = 0; x < tiles.GetLength(0); x++)
-            {
-                for (int y = 0; y < tiles.GetLength(1); y++)
-                {
-                    GameObject tileObject = new GameObject();
-                    Tile tile = tileObject.AddComponent<Tile>();
-                    tiles[x, y] = tile;
-                }
-            }
-            return tiles;
         }
 
         [Test]
         public void GetTile_WhenCalled_GetTileAtGivenPositionInGridSimplePasses()
         {
-            _gameBoard.SetTiles(_tiles);
+            _gameBoard.SetTiles(_tilesReference);
 
-            Assert.AreEqual(_gameBoard.GetTile(_tileGridPosition), _tiles[_tileGridPosition.x, _tileGridPosition.y]);
+            Tile referenceTile = _tilesReference[_tileGridPosition.x, _tileGridPosition.y];
+            Tile actualTile = _gameBoard.GetTile(_tileGridPosition);
+
+            Assert.AreEqual(actualTile, referenceTile);
         }
     }
 }
