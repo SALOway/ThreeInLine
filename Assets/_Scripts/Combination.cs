909b242e4eea1a6cf5ba0a60c6b490628f23add0
Yehor <zhora.levchenkoko@gmail.com>
2023-06-30 20:20:24 +0300

    Update Combination.cs

--- Assets/_Scripts/Combination.cs
+++ Assets/_Scripts/Combination.cs
@@ -1,17 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
 public class Combination
 {
-    public IList<Tile> Tiles;
-    public CombinationType Type;
+    public IReadOnlyList<Tile> Tiles;
+    public readonly CombinationType Type;
     public int Count => Tiles.Count;
 
-    public Combination(IList<Tile> tiles, CombinationType type)
+    public Combination(IReadOnlyList<Tile> tiles, CombinationType type)
     {
         Tiles = tiles;
         Type = type;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Combination other)
+        {
+            bool typesEqual = Type == other.Type;
+            bool countEqual = Count == other.Count;
+            bool tilesEqual = true;
+            if (countEqual)
+            {
+                for (int i = 0; i < other.Count; i++)
+                {
+                    bool hasEqualTile = false;
+                    for (int j = 0; j < other.Count; j++)
+                    {
+                        hasEqualTile = ReferenceEquals(Tiles[i], other.Tiles[i]);
+                        if (hasEqualTile) break;
+                    }
+                    if (!hasEqualTile) tilesEqual = false;
+                }
+            }
+            return typesEqual && countEqual && tilesEqual;
+        }
+
+        return false;
+    }
+    public static bool operator ==(Combination left, Combination right)
+    {
+        return Equals(left, right);
+    }
+    public static bool operator !=(Combination left, Combination right)
+    {
+        return !Equals(left, right);
+    }
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 31 + Type.GetHashCode();
+            foreach (var tile in Tiles)
+            {
+                hash = hash * 31 + tile.GetHashCode();
+            }
+            return hash;
+        }
     }
 }
 
