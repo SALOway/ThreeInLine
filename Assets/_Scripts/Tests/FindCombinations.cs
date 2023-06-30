dac4b4cf40918a1b9e2cc2e73cc77204151c8ca4
Yehor <zhora.levchenkoko@gmail.com>
2023-06-30 23:11:57 +0300

    Update FindCombinations.cs
    Separated horizontal and vertical combinations

--- Assets/_Scripts/Tests/FindCombinations.cs
+++ Assets/_Scripts/Tests/FindCombinations.cs
@@ -6,7 +6,8 @@ namespace Tests
     public class FindCombinations
     {
         GameBoard _gameBoard;
-        List<Combination> _expectedCombinations;
+        List<Combination> _expectedHorizontalCombinations;
+        List<Combination> _expectedVerticalCombinations;
 
         [SetUp]
         public void Setup()
@@ -23,33 +24,73 @@ namespace Tests
             _gameBoard = TestingUtilites.CreateGameBoard();
             _gameBoard.SetTiles(tiles);
 
+            #region Horizontal
             Tile[] tilesInHorizontalCombination = { tiles[0, 1], tiles[1, 1], tiles[2, 1] };
-            Combination expectedHorizontalCombination = new Combination(tilesInHorizontalCombination , CombinationType.Horizontal);
-            _expectedCombinations = new List<Combination>() { expectedHorizontalCombination };
-        }
+            Combination expectedHorizontalCombination = new Combination(tilesInHorizontalCombination, CombinationType.Horizontal);
+            _expectedHorizontalCombinations = new List<Combination>() { expectedHorizontalCombination };
+            #endregion
 
+            #region Vertical
+            _expectedVerticalCombinations = new List<Combination>();
+            #endregion
+
+        }
 
         [Test]
         public void FindAllCombinations_WhenCalled_GetAllCombinationsOnGameBoard()
         {
             List<Combination> allCombinations = _gameBoard.FindAllCombinations();
 
-            Assert.AreEqual(_expectedCombinations.Count, allCombinations.Count, "Invalid number of combinations");
-
-            for (int i = 0; i < allCombinations.Count; i++)
+            #region Horizontal Combinations
+            List<Combination> horizontalCombinations = new List<Combination>();
+            foreach (var combination in allCombinations)
             {
-                Combination expectedCombination = _expectedCombinations[i];
-                Combination combination = allCombinations[i];
+                if (combination.Type == CombinationType.Horizontal) horizontalCombinations.Add(combination);
+            }
 
-                Assert.AreEqual(expectedCombination.Count, combination.Count, $"Invalid combination length at index {i}");
-                
+            Assert.AreEqual(_expectedHorizontalCombinations.Count, horizontalCombinations.Count, "Invalid number of horizontal combinations");
+
+            for (int i = 0; i < horizontalCombinations.Count; i++)
+            {
+                Combination expectedCombination = _expectedHorizontalCombinations[i];
+                Combination combination = horizontalCombinations[i];
+
+                Assert.AreEqual(expectedCombination.Count, combination.Count, $"Invalid horizontal combination length at index {i}");
+
                 for (int j = 0; j < combination.Count; j++)
                 {
                     int expectedType = expectedCombination.Tiles[j].Type;
                     int type = combination.Tiles[j].Type;
-                    Assert.AreEqual(expectedType, type, "Tile mismatch at index " + j);
+                    Assert.AreEqual(expectedType, type, "Tile mismatch in horizontal combination at index " + j);
                 }
             }
+            #endregion
+
+            #region Vertical Combinations
+            List<Combination> verticalCombinations = new List<Combination>();
+            foreach (var combination in allCombinations)
+            {
+                if (combination.Type == CombinationType.Vertical) verticalCombinations.Add(combination);
+            }
+
+            Assert.AreEqual(_expectedVerticalCombinations.Count, verticalCombinations.Count, "Invalid number of vertical combinations");
+
+            for (int i = 0; i < verticalCombinations.Count; i++)
+            {
+                Combination expectedCombination = _expectedHorizontalCombinations[i];
+                Combination combination = verticalCombinations[i];
+
+                Assert.AreEqual(expectedCombination.Count, combination.Count, $"Invalid vertical combination length at index {i}");
+
+                for (int j = 0; j < combination.Count; j++)
+                {
+                    int expectedType = expectedCombination.Tiles[j].Type;
+                    int type = combination.Tiles[j].Type;
+                    Assert.AreEqual(expectedType, type, "Tile mismatch in vertical combination at index " + j);
+                }
+            }
+            #endregion
+
         }
     }
 }
