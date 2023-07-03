e9f08d87af99bfcf13bbc489a7f9d7a6d4ed51e0
Yehor <zhora.levchenkoko@gmail.com>
2023-07-03 18:33:18 +0300

    Update GameBoard.cs

--- Assets/_Scripts/GameBoard.cs
+++ Assets/_Scripts/GameBoard.cs
@@ -167,32 +167,138 @@ public class GameBoard : MonoBehaviour
         return verticalCombinations;
     }
 
-    public NeighborTiles GetNeighborTiles(Vector3Int tileGridPosition)
+    public int CountAllTilesWithAvailableMoves()
+    {
+        HashSet<Tile> uniqueTilesWithMove = new HashSet<Tile>();
+        for (int x = 0; x < _tilesInGrid.GetLength(0); x++)
+        {
+            for (int y = 0; y < _tilesInGrid.GetLength(1); y++)
+            {
+                Vector3Int tileGridPosition = new Vector3Int(x, y, 0);
+                FindTilesWithMoveAt(tileGridPosition).ForEach(tile => uniqueTilesWithMove.Add(tile));
+            }
+        }
+
+
+        foreach (Tile tile in uniqueTilesWithMove)
+        {
+            Debug.Log($"Tile Position: ({tile.GridPosition.x},{tile.GridPosition.y})");
+        }
+
+        int movesCounter = uniqueTilesWithMove.Count;
+        return movesCounter;
+    }
+
+    private List<Tile> FindTilesWithMoveAt(Vector3Int tileGridPosition)
+    {
+        List<Tile> tilesWithMove = new List<Tile>();
+        NeighborTiles neighborTiles = GetNeighborTilesAt(GetTileAt(tileGridPosition));
+
+        if (neighborTiles.HasCornerNeighbors)
+        {
+            if (neighborTiles.Corner.Count > 1)
+            {
+                tilesWithMove.AddRange(FindTilesForCornerMove(neighborTiles));
+            }
+
+            if (neighborTiles.HasHorizontalNeighbors)
+            {
+                tilesWithMove.AddRange(FindTilesForHorizontalMove(neighborTiles));
+            }
+
+            if (neighborTiles.HasVerticalNeighbors)
+            {
+                tilesWithMove.AddRange(FindTilesForVerticalMove(neighborTiles));
+            }
+        }
+        return tilesWithMove;
+    }
+
+    private List<Tile> FindTilesForCornerMove(NeighborTiles neighborTiles)
+    {
+        List<Tile> tilesWithMove = new List<Tile>();
+        foreach (Tile cornerTile in neighborTiles.Corner)
+        {
+            if (cornerTile == null) continue;
+            foreach (Tile other in neighborTiles.Corner)
+            {
+                if (other == null || cornerTile.GridPosition == other.GridPosition) continue;
+                if (cornerTile.GridPosition.y == other.GridPosition.y || cornerTile.GridPosition.x == other.GridPosition.x)
+                {
+                    tilesWithMove.Add(GetTileAt(neighborTiles.BaseTileGridPosition));
+                }
+            }
+        }
+        return tilesWithMove;
+    }
+
+    private List<Tile> FindTilesForHorizontalMove(NeighborTiles neighborTiles)
+    {
+        List<Tile> tilesWithMove = new List<Tile>();
+        foreach (Tile horizontlaTile in neighborTiles.Horizontal)
+        {
+            if (horizontlaTile == null) continue;
+            foreach (Tile cornerTile in neighborTiles.Corner)
+            {
+                if (cornerTile == null) continue;
+
+                if (horizontlaTile.GridPosition.x != cornerTile.GridPosition.x)
+                {
+                    tilesWithMove.Add(cornerTile);
+                }
+            }
+        }
+        return tilesWithMove;
+    }
+    
+    private List<Tile> FindTilesForVerticalMove(NeighborTiles neighborTiles)
+    {
+        List<Tile> tilesWithMove = new List<Tile>();
+        foreach (Tile verticalTile in neighborTiles.Vertical)
+        {
+            if (verticalTile == null) continue;
+            foreach (Tile cornerTile in neighborTiles.Corner)
+            {
+                if (cornerTile == null) continue;
+
+                if (verticalTile.GridPosition.y != cornerTile.GridPosition.y)
+                {
+                    tilesWithMove.Add(cornerTile);
+                }
+            }
+        }
+        return tilesWithMove;
+    }
+
+    public NeighborTiles GetNeighborTilesAt(Tile tile)
     {
         List<Tile> horizontalNeighbors = new List<Tile>();
         List<Tile> verticalNeighbors = new List<Tile>();
         List<Tile> cornerNeighbors = new List<Tile>();
-        for (int x = tileGridPosition.x - 1; x < tileGridPosition.x + 1; x++)
+        for (int x = tile.GridPosition.x - 1; x <= tile.GridPosition.x + 1; x++)
         {
-            for (int y = tileGridPosition.y - 1; y < tileGridPosition.y + 1; y++)
+            for (int y = tile.GridPosition.y - 1; y <= tile.GridPosition.y + 1; y++)
             {
                 Vector3Int neighborTileGridPosition = new Vector3Int(x, y, 0);
-                if (neighborTileGridPosition == tileGridPosition) continue;
+                Tile neighborTile = GetTileAt(neighborTileGridPosition);
 
-                if (neighborTileGridPosition.y == tileGridPosition.y)
+                if (neighborTile == null) continue;
+                if (neighborTile.GridPosition == tile.GridPosition) continue;
+
+                if (neighborTile.GridPosition.y == tile.GridPosition.y)
                 {
-                    horizontalNeighbors.Add(GetTileAt(neighborTileGridPosition));
+                    if (neighborTile.Type == tile.Type) horizontalNeighbors.Add(GetTileAt(neighborTileGridPosition));
                     continue;
                 }
-                if (neighborTileGridPosition.x == tileGridPosition.x)
+                if (neighborTile.GridPosition.x == tile.GridPosition.x)
                 {
-                    verticalNeighbors.Add(GetTileAt(neighborTileGridPosition));
+                    if (neighborTile.Type == tile.Type) verticalNeighbors.Add(GetTileAt(neighborTileGridPosition));
                     continue;
                 }
-                cornerNeighbors.Add(GetTileAt(neighborTileGridPosition));
+                if (neighborTile.Type == tile.Type) cornerNeighbors.Add(GetTileAt(neighborTileGridPosition));
             }
         }
-        NeighborTiles neighbors = new NeighborTiles(horizontalNeighbors, verticalNeighbors, cornerNeighbors);
+        NeighborTiles neighbors = new NeighborTiles(tile.GridPosition, horizontalNeighbors, verticalNeighbors, cornerNeighbors);
         return neighbors;
     }
 }
