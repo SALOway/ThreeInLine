fc11a1837cfe62428ba83a39d8550e37df8b9020
Yehor <zhora.levchenkoko@gmail.com>
2023-07-09 12:41:06 +0300

    Update GameController.cs

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -7,5 +7,26 @@ using UnityEngine;
 /// </summary>
 public class GameController : MonoBehaviour
 {
+    [SerializeField] GameBoard _board;
 
+    private void Start()
+    {
+        // InitializeGame();
+    }
+
+    public void InitializeGame()
+    {
+        _board.TileGrid = new TileGrid(5, 6);
+
+        List<Combination> allCombinations = _board.FindAllCombinations();
+        List<Tile> allTilesWithMove = _board.FindAllTilesWithMove();
+
+        while (allCombinations.Count > 0 || allTilesWithMove.Count == 0)
+        {
+            _board.TileGrid = new TileGrid(5, 6);
+
+            allCombinations = _board.FindAllCombinations();
+            allTilesWithMove = _board.FindAllTilesWithMove();
+        }
+    }
 }
